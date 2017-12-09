bdad619fe9e6b7bf7399f4c8fd5f848b4ad214ea
m-griffin <mrmisticismo@hotmail.com>
2017-12-09 05:42:22 -0600

    Updated Socket Errors / disconnects to return errors codes to Callback for better socket shutdown and processing flow. Note: WIP, Need to update error codes to reflect what happened more accurately.

--- src/io_service.cpp
+++ src/io_service.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <system_error>
 
 IOService::IOService()
     : m_is_active(false)
@@ -38,33 +39,39 @@ void IOService::run()
              * Handle Read Service if Data is Available.
              */
             if (job_work->getServiceType() == SERVICE_TYPE_READ)
-            {
-                std::error_code ec (0, std::generic_category());
+            {                
                 // If Data Available, read, then ppulate buffer, and remove
                 // Otherwise keep polling till data is available.
                 int result = job_work->getSocket()->poll();
                 if (result > 0)
                 {
-                    memset(&msg_buffer, 0, sizeof(msg_buffer));
+                    memset(&msg_buffer, 0, MAX_BUFFER_SIZE);
                     int length = job_work->getSocket()->recvSocket(msg_buffer);
                     if(length < 0)
                     {
                         // Error - Lost Connection
                         std::cout << "tcp_connection async_read - lost connection!: " << length << std::endl;
                         job_work->getSocket()->setInactive();
+                        callback_function_handler run_callback(job_work->getCallback());
+                        std::error_code lost_connect_error_code (1, std::system_category());
+                        run_callback(lost_connect_error_code);
                         m_service_list.remove(i);
                     }
                     else
                     {
                         job_work->setBuffer((unsigned char *)msg_buffer);
                         callback_function_handler run_callback(job_work->getCallback());
-                        run_callback(ec);
+                        std::error_code success_code (0, std::generic_category());
+                        run_callback(success_code);
                         m_service_list.remove(i);
                     }
                 }
                 else if (result == -1)
                 {
                     std::cout << "SDL Poll Error!" << std::endl;
+                    callback_function_handler run_callback(job_work->getCallback());
+                    std::error_code lost_connect_error_code (1, std::system_category());
+                    run_callback(lost_connect_error_code);
                     m_service_list.remove(i);
                 }
             }
@@ -74,8 +81,7 @@ void IOService::run()
              */
             else if (job_work->getServiceType() == SERVICE_TYPE_WRITE)
             {
-               // std::cout << "* SERVICE_TYPE_WRITE" << std::endl;
-                std::error_code ec (0, std::generic_category());
+                // std::cout << "* SERVICE_TYPE_WRITE" << std::endl;                
                 int result = job_work->getSocket()->sendSocket(
                                  (unsigned char*)job_work->getWriteSequence().c_str(),
                                  job_work->getWriteSequence().size());
@@ -85,12 +91,16 @@ void IOService::run()
                     // Error - Lost Connection
                     std::cout << "tcp_connection async_write - lost connection!" << std::endl;
                     job_work->getSocket()->setInactive();
+                    callback_function_handler run_callback(job_work->getCallback());
+                    std::error_code lost_connect_error_code (1, std::system_category());
+                    run_callback(lost_connect_error_code);
                     m_service_list.remove(i);
                 }
                 else
                 {
                     callback_function_handler run_callback(job_work->getCallback());
-                    run_callback(ec);
+                    std::error_code success_code (0, std::generic_category());
+                    run_callback(success_code);
                     m_service_list.remove(i);
                 }
             }
